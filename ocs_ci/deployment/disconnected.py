53fc0d50568e32bf43f214f918bbf32b9d30b8e5
Daniel Horak <dahorak@redhat.com>
2021-01-27 09:35:53 +0100

    TMP: discon.cl.: conditionally install opm tool
    Signed-off-by: Daniel Horak <dahorak@redhat.com>

--- ocs_ci/deployment/disconnected.py
+++ ocs_ci/deployment/disconnected.py
@@ -10,7 +10,11 @@ import yaml
 
 from ocs_ci.framework import config
 from ocs_ci.ocs import constants
-from ocs_ci.ocs.exceptions import NotFoundError, UnsupportedOSType
+from ocs_ci.ocs.exceptions import (
+    CommandFailed,
+    NotFoundError,
+    UnsupportedOSType,
+)
 from ocs_ci.utility.utils import (
     create_directory_path,
     download_file,
@@ -76,55 +80,57 @@ def prepare_disconnected_ocs_deployment():
     logger.info("Prepare for disconnected OCS installation")
     if config.DEPLOYMENT.get("live_deployment"):
 
-        # get opm tool
-        opm_owner_repo = "operator-framework/operator-registry"
-        opm_release_tag = "latest"
-        if opm_release_tag != "latest":
-            opm_release_tag = f"tags/{opm_release_tag}"
-        opm_releases_api_url = (
-            f"https://api.github.com/repos/{opm_owner_repo}/releases/{opm_release_tag}"
-        )
-        if config.AUTH.get("github"):
-            github_auth = (
-                config.AUTH["github"].get("username"),
-                config.AUTH["github"].get("token"),
-            )
-            logger.debug(f"Using github authentication (user: {github_auth[0]})")
-        else:
-            github_auth = None
-            logger.warning(
-                "Github credentials are not provided in data/auth.yaml file. "
-                "You might encounter issues with accessing github api as it "
-                "have very strict rate limit for unauthenticated requests "
-                "(60 requests per hour). Please check docs/getting_started.md "
-                "file to find how to configure github authentication."
-            )
-        release_data = json.loads(
-            get_url_content(opm_releases_api_url, auth=github_auth)
-        )
-
-        if platform.system() == "Darwin":
-            opm_asset_name = "darwin-amd64-opm"
-        elif platform.system() == "Linux":
-            opm_asset_name = "linux-amd64-opm"
-        else:
-            raise UnsupportedOSType
-
-        for asset in release_data["assets"]:
-            if asset["name"] == opm_asset_name:
-                opm_download_url = asset["browser_download_url"]
-                break
-        else:
-            raise NotFoundError(
-                f"opm binary for selected type {opm_asset_name} was not found"
+        try:
+            opm_version = exec_cmd("opm version")
+            logger.info(f"opm tool is available: {opm_version.stdout}")
+        except CommandFailed:
+            logger.info("installing opm tool")
+            opm_owner_repo = "operator-framework/operator-registry"
+            opm_release_tag = "latest"
+            if opm_release_tag != "latest":
+                opm_release_tag = f"tags/{opm_release_tag}"
+            opm_releases_api_url = f"https://api.github.com/repos/{opm_owner_repo}/releases/{opm_release_tag}"
+            if config.AUTH.get("github"):
+                github_auth = (
+                    config.AUTH["github"].get("username"),
+                    config.AUTH["github"].get("token"),
+                )
+                logger.debug(f"Using github authentication (user: {github_auth[0]})")
+            else:
+                github_auth = None
+                logger.warning(
+                    "Github credentials are not provided in data/auth.yaml file. "
+                    "You might encounter issues with accessing github api as it "
+                    "have very strict rate limit for unauthenticated requests "
+                    "(60 requests per hour). Please check docs/getting_started.md "
+                    "file to find how to configure github authentication."
+                )
+            release_data = json.loads(
+                get_url_content(opm_releases_api_url, auth=github_auth)
             )
 
-        prepare_bin_dir()
-        bin_dir = None
-        bin_dir = os.path.expanduser(bin_dir or config.RUN["bin_dir"])
-        download_file(opm_download_url, os.path.join(bin_dir, "opm"))
-        cmd = f"chmod +x {os.path.join(bin_dir, 'opm')}"
-        exec_cmd(cmd)
+            if platform.system() == "Darwin":
+                opm_asset_name = "darwin-amd64-opm"
+            elif platform.system() == "Linux":
+                opm_asset_name = "linux-amd64-opm"
+            else:
+                raise UnsupportedOSType
+
+            for asset in release_data["assets"]:
+                if asset["name"] == opm_asset_name:
+                    opm_download_url = asset["browser_download_url"]
+                    break
+            else:
+                raise NotFoundError(
+                    f"opm binary for selected type {opm_asset_name} was not found"
+                )
+
+            prepare_bin_dir()
+            bin_dir = None
+            bin_dir = os.path.expanduser(bin_dir or config.RUN["bin_dir"])
+            download_file(opm_download_url, os.path.join(bin_dir, "opm"))
+            cmd = f"chmod +x {os.path.join(bin_dir, 'opm')}"
+            exec_cmd(cmd)
 
         raise NotImplementedError(
             "Disconnected installation from live is not implemented!"
