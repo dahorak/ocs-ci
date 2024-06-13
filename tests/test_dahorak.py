46f1bf9efec1e801029d3d32042d7590e0477def
Daniel Horak <dahorak@redhat.com>
2024-06-13 09:03:18 +0200

    add dummy test case
    Signed-off-by: Daniel Horak <dahorak@redhat.com>

---
+++ tests/test_dahorak.py
@@ -0,0 +1,9 @@
+from ocs_ci.framework.testlib import acceptance
+
+
+@acceptance
+def test_require_virtual_hosts_for_rgw(virtual_host_style_acess_for_rgw_session):
+    """
+    dummy test
+    """
+    assert True, "dummy test"
