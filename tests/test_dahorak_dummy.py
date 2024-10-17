209c347a66c0a8a6a4fb8a6050e5f83a007e0799
Daniel Horak <dahorak@redhat.com>
2024-10-17 10:44:41 +0200

    TMP: dummy dahorak's test
    Signed-off-by: Daniel Horak <dahorak@redhat.com>

---
+++ tests/test_dahorak_dummy.py
@@ -0,0 +1,14 @@
+import logging
+
+# import pytest
+
+# from ocs_ci.framework import config
+
+log = logging.getLogger(__name__)
+
+
+def test_dahorak():
+    """
+    Dummy test
+    """
+    log.info("Dummy dahorak's test")
